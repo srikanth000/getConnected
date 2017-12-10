243f595ef8783090d176a72f6cffa3c17a7e3772
srikanth000 <margamsrikanth7@gmail.com>
2017-12-10 10:03:29 +0530

    CRU operations added to the blog

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+	def index
+		@article=Article.all
+	end
+
 	def new
 		@article=Article.new
 	end
@@ -16,6 +20,22 @@ class ArticlesController < ApplicationController
 	def show
 		@article = Article.find(params[:id])
 	end
+	def edit
+		@article =Article.find(params[:id])
+	end
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			flash[:notice]="Article successfully updated"
+			redirect_to article_path(@article)
+		else
+			render 'edit'
+		end
+	end
+			
+	
+
+
 	private
 
 		def article_params
