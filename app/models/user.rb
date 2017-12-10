bcd941ab0ca96fb771776037353eceb44e0d6a8d
srikanth000 <margamsrikanth7@gmail.com>
2017-12-10 22:53:20 +0530

    signup completer

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User <ActiveRecord::Base
 	VALID_EMAIL_REGEX=/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email,presence:true,length: {minimum:3,maximum:25},uniqueness:{case_sensitive: false},
 	format: { with: VALID_EMAIL_REGEX}
+
 	has_secure_password
 
 end
